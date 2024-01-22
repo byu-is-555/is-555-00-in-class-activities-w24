6f245a1d32c6bf5d54d07820b35a536ab7960efc
david-wilson-175 <35510393+david-wilson-175@users.noreply.github.com>
2024-01-22 11:02:59 -0700

    Create 3b - data cleaning in class.R

---
+++ 3b - data cleaning in class.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+
+raw <- read_csv('https://www.dropbox.com/scl/fi/ug8tbxsdd2qtsfqwnnox1/dollar_store.csv?rlkey=fu36g6uhfpx8u644d1rpsq11i&dl=1')
+
+
+# Convert to number formats: price, star_rating, review_count, stock_status, unit_size
+#   Goals:   (1) Don't lose information
+#            (2) Missing where appropriate, but only where appropriate
+
+
+
+# Create usable brand and product_name columns from the product_info column
+#   Hints:  (1) Ensure missingness is appropriate. 
+#           (2) If possible, exploit any repeated structure in the column
+
+
+# Create usable brand and product_name columns from the product_info column
+#   Hints:  (1) Ensure missingness is appropriate. 
+#           (2) If possible, exploit any repeated structure in the column
+
+
+# Convert date columns to date and/or date-time columns, then calculate how many
+# days each product sat on the shelf before its first sale.
+
+
